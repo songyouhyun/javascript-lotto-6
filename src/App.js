dcd960d39b11c864a2835512d0d6a3b2eed37e93
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-17 22:03:41 +0900

    feat: 구입 금액에 해당하는만큼 로또 발행

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import {Console} from "@woowacourse/mission-utils";
+import {Console, Random} from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
   async play() {
-    const purchaseAmount = await Console.readLineAsync('구입금액을 입력해주세요.\n')
+    const purchaseAmount = await Console.readLineAsync('구입금액을 입력해주세요.\n');
+    const lottoCount = purchaseAmount / 1000;
+
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+    for (let i = 0; i < lottoCount; i++) {
+      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const lotto = new Lotto(randomNumbers);
+      Console.print(lotto.numbers);
+    }
+
   }
 }
 
