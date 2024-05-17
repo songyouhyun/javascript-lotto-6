975c1bda15c04bdd37418f738b828408b4e43a23
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-17 17:52:42 +0900

    feat: 로또 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import {Console} from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await Console.readLineAsync('구입금액을 입력해주세요.\n')
+  }
 }
 
 export default App;
